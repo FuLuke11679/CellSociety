768db8a261c49027b00b79b43032996e1af323fc
Luke Fu <lmf59@duke.edu>
2025-02-05 00:44:09 -0500

    Added testing for GridTest

---
+++ src/test/java/cellsociety/GridTest.java
@@ -0,0 +1,24 @@
+package cellsociety;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GridTest {
+
+  @Test
+  void initializeGrid() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void getColor() {
+  }
+
+  @Test
+  void getLength() {
+  }
+}
