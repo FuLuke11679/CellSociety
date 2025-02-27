4ce93bdb81e72d4ad83a90a1c609ca371afbcf3e
Luke Fu <lmf59@duke.edu>
2025-02-27 17:50:56 -0500

    Feature CELL-48A: Implementing Toroidal Edge Handler Interface

--- src/main/java/cellsociety/model/grid/handler/ToroidalEdgeHandler.java
+++ src/main/java/cellsociety/model/grid/handler/ToroidalEdgeHandler.java
@@ -1,21 +1,19 @@
-package cellsociety.model.grid;
+package cellsociety.model.grid.handler;
 
 import cellsociety.model.cell.Cell;
+import cellsociety.model.grid.Grid;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToroidalEdgeHandler implements EdgeHandler {
-  public List<Cell> getNeighbors(int row, int col, Grid grid, List<int[]> offsets) {
-  List<Cell> neighbors = new ArrayList<>();
-    int[] dx = {-1, -1, -1, 1, 1, 1, 0, 0};
-    int[] dy = {-1, 0, 1, 0, -1, 1, 1, -1};
+  public List<Cell> handleNeighbors(int row, int col, Grid grid, List<int[]> offsets) {
+    List<Cell> neighbors = new ArrayList<>();
+    int numRows = grid.getRows();
+    int numCols = grid.getColumns();
 
-    for (int i = 0; i < dx.length; i++) {
-      int numRows = grid.getRows();
-      int numCols = grid.getColumns();
-      int newRow = (row + dx[i] + numRows) % numRows;  // Wrap row
-      int newCol = (col + dy[i] + numCols) % numCols;  // Wrap column
-
+    for(int[] offset : offsets) {
+      int newRow = (row + offset[0] + numRows) % numRows;
+      int newCol = (col + offset[1] + numCols) % numCols;
       neighbors.add(grid.getCell(newRow, newCol));
     }
     return neighbors;
