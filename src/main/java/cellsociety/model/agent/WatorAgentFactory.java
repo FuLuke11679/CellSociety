2719591850a6e4813b10b12022399349b82ed218
Daniel Rodriguez-Florido <dr285@duke.edu>
2025-03-05 15:53:01 -0500

    Adds javadoc, fixes XML for wator

--- src/main/java/cellsociety/model/agent/WatorAgentFactory.java
+++ src/main/java/cellsociety/model/agent/WatorAgentFactory.java
@@ -9,6 +9,12 @@ import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
+/**
+ * Author: Daniel Rodriguez-Florido
+ *
+ * Agent factory for the WatorWorld Ruleset (instantiates either fish or shark)
+ */
 public class WatorAgentFactory {
 
   private static final String PROPERTY_FILE_NAME = "cellsociety/agents.properties";
@@ -21,10 +27,19 @@ public class WatorAgentFactory {
 
   private final Map<String, String> params;
 
+  /**
+   * Constructor to take in the params of the agents
+   * @param params The parameters we wish to fill the agents with
+   */
   public WatorAgentFactory(Map<String, String> params) {
     this.params = params;
   }
 
+  /**
+   * Creates a new WatorAgent based on the state of the cell
+   * @param agentType The type of agent we wish to create
+   * @return The new WatorAgent
+   */
   public WatorAgent createWatorAgent(CellState agentType) {
     Properties myProperties = new Properties();
     PropertiesLoader.loadPropertiesFolder(PROPERTY_FILE_NAME, myProperties);
@@ -35,17 +50,17 @@ public class WatorAgentFactory {
 
       try {
         Constructor<?> constructor = clazz.getConstructor(int.class, int.class);
-        int param1 = Integer.parseInt(params.get(SHARK_ENERGY_GAIN_NAME));
-        int param2 = Integer.parseInt(params.get(SHARK_BREED_PARAM_NAME));
+        int param1 = Integer.parseInt(params.getOrDefault(SHARK_ENERGY_GAIN_NAME, "5"));
+        int param2 = Integer.parseInt(params.getOrDefault(SHARK_BREED_PARAM_NAME, "3"));
         return (WatorAgent) constructor.newInstance(param1, param2);
       } catch (NoSuchMethodException e) {
-        log.error("No constructor for ruleset.");
+        log.warn("No constructor for agent of two params. Trying other constructor.");
       } catch (Exception e) {
         log.error("Could not instantiate Agent with two parameters: {}", agentType);
       }
 
       Constructor<?> constructor = clazz.getConstructor(int.class);
-      int param1 = Integer.parseInt(params.get(FISH_BREED_PARAM_NAME));
+      int param1 = Integer.parseInt(params.getOrDefault(FISH_BREED_PARAM_NAME, "5"));
       return (WatorAgent) constructor.newInstance(param1);
 
     } catch (ClassNotFoundException | NoSuchMethodException e) {
